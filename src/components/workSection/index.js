775105a5fcd288006f7dc5277b96da4b6215c02a
Diego <diego.berardi02@gmail.com>
2022-04-11 20:02:56 +0200

    added: chess game

--- src/components/workSection/index.js
+++ src/components/workSection/index.js
@@ -8,6 +8,7 @@ import hackerNewsImg from "../../assets/img/hackerNews.png";
 import calculatorImg from "../../assets/img/calculator.png";
 import ticTacToeImg from "../../assets/img/TicTacToe.png";
 import snakeGameImg from "../../assets/img/Screenshot-Snake-Game.png";
+import chessGameImg from "../../assets/img/chessGame.png";
 
 //components
 import ProjectComponent from "../projectComponent";
@@ -37,11 +38,11 @@ const WorkSection = () => {
             The Movie Database's api: `}
         linkApi={"https://www.themoviedb.org/"}
         technologies={[
-          <FaReact className="react" key='react' />,
-          <AiFillHtml5 className="html" key='html' />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
-          <SiSass className="sass" key='sass' />,
+          <FaReact className="react" key="react" />,
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
+          <SiSass className="sass" key="sass" />,
         ]}
         linkProject={"https://netflix-clone-database.netlify.app/"}
         linkGitHub={"https://github.com/Diego-Berardi/Netflix-clone"}
@@ -54,11 +55,11 @@ const WorkSection = () => {
             of the New York Times. The project is developed with the NYT's api:`}
         linkApi={"https://new-york-times-clone.netlify.app/"}
         technologies={[
-          <FaReact className="react" key='react' />,
-          <AiFillHtml5 className="html" key='html' />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
-          <SiSass className="sass" key='sass' />,
+          <FaReact className="react" key="react" />,
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
+          <SiSass className="sass" key="sass" />,
         ]}
         linkProject={"https://new-york-times-clone.netlify.app/"}
         linkGitHub={"https://github.com/Diego-Berardi/NYT-clone-Start2Impact"}
@@ -72,10 +73,10 @@ const WorkSection = () => {
             show the last tech related news. The project is developed with the HackerNews's api: `}
         linkApi={"https://github.com/HackerNews/API"}
         technologies={[
-          <AiFillHtml5 className="html" key='html' />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
-          <SiSass className="sass" key='sass' />,
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
+          <SiSass className="sass" key="sass" />,
         ]}
         linkProject={"https://hacker-news-start2impact.netlify.app/"}
         linkGitHub={"https://github.com/Diego-Berardi/hacker-news-Start2Impact"}
@@ -83,12 +84,38 @@ const WorkSection = () => {
       />
       <ProjectComponent
         fadeInAni={"fade-left"}
+        title={"Snake Game"}
+        textP={`Snake game project, move the snake with your arrow keys, go eat the green cube and try to make the beast score!`}
+        technologies={[
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
+        ]}
+        linkProject={"https://diego-berardi.github.io/Snake-Game/"}
+        linkGitHub={"https://github.com/Diego-Berardi/Snake-game"}
+        imgProject={snakeGameImg}
+      />
+      <ProjectComponent
+        fadeInAni={"fade-right"}
+        title={"Chess Game"}
+        textP={`Virtual chess board where you can play chess 1vs1 offline.`}
+        technologies={[
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
+        ]}
+        linkProject={"https://diego-berardi.github.io/Chess-Game/"}
+        linkGitHub={"https://github.com/Diego-Berardi/Chess-game"}
+        imgProject={chessGameImg}
+      />
+      <ProjectComponent
+        fadeInAni={"fade-left"}
         title={"Calculator JavaScript"}
         textP={`Simple calculator built with HTML, CSS and JavaScript `}
         technologies={[
-          <AiFillHtml5 className="html" key='html'  />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
         ]}
         linkProject={"https://diego-berardi.github.io/Simple-Calculator/"}
         linkGitHub={"https://github.com/Diego-Berardi/Simple-Calculator"}
@@ -100,26 +127,13 @@ const WorkSection = () => {
         textP={`Basic Tic Tac Toe game. You can choose to play in 2 players or alone
             vs the unbeateble AI `}
         technologies={[
-          <AiFillHtml5 className="html" key='html' />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
+          <AiFillHtml5 className="html" key="html" />,
+          <IoLogoCss3 className="css" key="css" />,
+          <IoLogoJavascript className="javascript" key="javascript" />,
         ]}
         linkProject={"https://diego-berardi.github.io/Tic-Tac-Toe/"}
         linkGitHub={"https://github.com/Diego-Berardi/Tic-Tac-Toe"}
         imgProject={ticTacToeImg}
-      />
-      <ProjectComponent
-        fadeInAni={"fade-left"}
-        title={"Snake Game"}
-        textP={`Snake game project, move the snake with your arrow keys, go eat the green cube and try to make the beast score!`}
-        technologies={[
-          <AiFillHtml5 className="html" key='html' />,
-          <IoLogoCss3 className="css" key='css' />,
-          <IoLogoJavascript className="javascript" key='javascript' />,
-        ]}
-        linkProject={"https://diego-berardi.github.io/Snake-Game/"}
-        linkGitHub={"https://github.com/Diego-Berardi/Snake-game"}
-        imgProject={snakeGameImg}
       />
     </section>
   );
